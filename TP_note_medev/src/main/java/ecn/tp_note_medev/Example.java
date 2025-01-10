b595a8923f39c4e9ca563f3478ce84cd78b83850
RBoutroux <remibtrx@gmail.com>
2025-01-10 17:24:13 +0100

    [feat] setting up project with ant

---
+++ TP_note_medev/src/main/java/ecn/tp_note_medev/Example.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ecn.tp_note_medev;
+
+/**
+ *
+ * @author remib
+ */
+public class Example {
+    public Example(int hihi){
+        //hehe
+    }
+    
+    public int returnThree(){
+        return 3;
+    }
+}
---
+++ TP_note_medev/src/test/java/ecn/tp_note_medev/ExampleTest.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package ecn.tp_note_medev;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author remib
+ */
+public class ExampleTest {
+    
+    public ExampleTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of returnThree method, of class Example.
+     */
+    @Test
+    public void testReturnThree() {
+        System.out.println("returnThree");
+        Example instance = null;
+        int expResult = 3;
+        int result = instance.returnThree();
+        assertEquals(expResult, result);
+    }
+    
+}
