2d3dfae2a27f9abc47644bd24833dd4188612cbf
QuentinBus <quentin.bussard@eleves.ec-nantes.fr>
2025-01-13 10:16:18 +0100

    create isFlipPossible

--- TP_note_medev/src/main/java/ecn/tp_note_medev/Joueur.java
+++ TP_note_medev/src/main/java/ecn/tp_note_medev/Joueur.java
@@ -4,6 +4,8 @@
  */
 package ecn.tp_note_medev;
 
+import java.util.Objects;
+
 /**
  *
  * @author Quent
@@ -29,5 +31,28 @@ public class Joueur {
 
     public Boolean getCanPlay() {
         return canPlay;
+    }
+    
+    
+    /**
+     * Fonction recursive qui teste si dans une direction donnée il est possible de retourner des pions
+     * @param posX la ligne actuelle
+     * @param posY la colonne actuelle
+     * @param dirX la direction verticale 0, 1 ou -1
+     * @param dirY la direction horyzontale 0, 1 ou -1
+     * @return true si on peut retourner qqch,  false sinon
+     */
+    private Boolean isFlipPossible(int posX,int posY, int dirX, int dirY){
+        int newX=posX+dirX;
+        int newY=posY+dirY;
+        if(newX>0 && newX<8 && newY>0 && newX<8){
+            Pion newPion =partie.getPions()[newX][newY];
+            if(newPion!=null && Objects.equals(isBlack, newPion.isBlack())){
+                return true;
+            }else if (newPion !=null){
+                return isFlipPossible(newX, newY, dirX, dirY);
+            }
+        }
+        return false;
     }
 }
